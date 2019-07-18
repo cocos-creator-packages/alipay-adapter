46bc3a028d93e9242830b627eb8d15a0e2fda361
PP <chinese1236@qq.com>
2019-07-18 12:44:10 +0800

    support release mode

--- alipaygame/game.js
+++ alipaygame/game.js
@@ -2,10 +2,10 @@ require('./adapter/my-builtin');
 var Parser = require('./adapter/xmldom/dom-parser');
 window.DOMParser = Parser.DOMParser;
 require('./adapter/engine/Platform');
-require('./adapter/my-downloader.js');
+require('./adapter/my-downloader');
 require('./src/settings');
 require('./main');
-require('./cocos2d-js');
+require(window._CCSettings.debug ? './cocos2d-js' : './cocos2d-js-min');
 require('./adapter/engine/index');
 
 // Adjust devicePixelRatio
