0066bdc3c2d4b6925d769741409837289b12541a
PP <chinese1236@qq.com>
2019-08-19 20:40:56 +0800

    refine error message (#5)

--- alipaygame/adapter/my-fs-utils.js
+++ alipaygame/adapter/my-fs-utils.js
@@ -38,12 +38,12 @@ function deleteFile (filePath, callback) {
     fs.unlink({
         filePath: filePath,
         success: function () {
-            cc.log('Removed local file ' + filePath + ' successfully!');
+            console.log('Removed local file ' + filePath + ' successfully!');
             callback && callback(null);
         },
         fail: function (res) {
-            console.warn(res.errMsg);
-            callback && callback(new Error(res.errMsg));
+            console.error('unlink file failed', res);
+            callback && callback(new Error(res.errorMessage));
         }
     });
 }
@@ -56,8 +56,8 @@ function downloadFile (remoteUrl, filePath, callback) {
             callback && callback(null, res.apFilePath);
         },
         fail: function (res) {
-            console.warn(res.errMsg);
-            callback && callback(new Error(res.errMsg), null);
+            console.error('download file failed', res);
+            callback && callback(new Error(res.errorMessage), null);
         }
     });
 }
@@ -67,12 +67,12 @@ function saveFile (srcPath, destPath, callback) {
         tempFilePath: srcPath,
         filePath: destPath,
         success: function (res) {
-            cc.log('save file finished:' + destPath);
+            console.log('save file finished:' + destPath);
             callback && callback(null, res.savedFilePath);
         },
         fail: function (res) {
-            cc.log('save file failed:' + res.errMsg);
-            callback && callback(new Error(res.errMsg), null);
+            console.error('save file failed', res);
+            callback && callback(new Error(res.errorMessage), null);
         }
     });
 }
@@ -84,12 +84,12 @@ function copyFile (srcPath, destPath, callback) {
         srcPath: srcPath,
         destPath: destPath,
         success: function () {
-            cc.log('copy file finished:' + destPath);
+            console.log('copy file finished:' + destPath);
             callback && callback(null);
         },
         fail: function (res) {
-            cc.log('copy file failed:' + res.errMsg);
-            callback && callback(new Error(res.errMsg));
+            console.error('copy file failed', res);
+            callback && callback(new Error(res.errorMessage));
         }
     });
 }
@@ -105,8 +105,8 @@ function writeFile (path, data, encoding, callback) {
             callback(null);
         } : undefined,
         fail: function (res) {
-            console.warn(res.errMsg);
-            callback && callback(new Error(res.errMsg));
+            console.error('write file failed', res);
+            callback && callback(new Error(res.errorMessage));
         }
     });
 }
@@ -134,8 +134,8 @@ function readFile (filePath, encoding, callback) {
             callback(null, res.data);
         } : undefined,
         fail: function (res) {
-            console.warn(res.errMsg);
-            callback && callback (new Error(res.errMsg), null);
+            console.error('read file failed', res);
+            callback && callback (new Error(res.errorMessage), null);
         }
     });
 }
@@ -151,7 +151,7 @@ function readDir (filePath, callback) {
             callback(null, res.files);
         } : undefined,
         fail: callback ? function (res) {
-            callback(new Error(res.errMsg), null);
+            callback(new Error(res.errorMessage), null);
         } : undefined
     });
 }
