e9be4d6cc599ab743193a624d9c25fb612787598
PP <chinese1236@qq.com>
2019-08-27 20:05:16 +0800

    require index script after subpackage loaded (#9)

--- alipaygame/adapter/engine/downloader.js
+++ alipaygame/adapter/engine/downloader.js
@@ -1,5 +1,11 @@
+let loadedSubPackages = {};
+
 cc.loader.downloader.loadSubpackage = function (name, completeCallback) {
     // Not support for now on Alipay platform
+    if (loadedSubPackages[name] !== true) {
+        require(`../../subpackages/${name}/index.js`);
+        loadedSubPackages[name] = true;
+    }
     completeCallback && completeCallback();
 };
 
